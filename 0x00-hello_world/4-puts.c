a501278cc6b73eba756150dd9256d5c495ea6d6d
MOSKLN <130203599+moskln@users.noreply.github.com>
2023-06-17 16:07:54 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
- * Return: Always 0.
- */
+* main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+* Return: Always 0.
+*/
 
 
 int main(void)
 
 
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
